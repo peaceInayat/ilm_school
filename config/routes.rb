0aa2968954275a55bd4b14142c4d0ccd186c72b5
peaceInayat <peace.inayat@gmail.com>
2017-05-08 10:51:09 +0530

    second major commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/contact_us'
+
+  get 'home/contact_us', :as=>'contact_us'
   get 'home/admissions'
+  get 'home/curriculum'
   get 'home/board_member'
   get 'home/our_staff'
   get 'home/faq'
+  get "home/brochure.pdf" => redirect("/brochure.pdf")
   get 'home/gallery'
   root 'home#welcome'
 end
